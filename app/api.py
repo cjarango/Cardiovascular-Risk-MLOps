426fe373696a5b7426523ab07541e594f8cdef30
cjarango <cjarango@uninorte.edu.co>
2026-04-03 19:07:17 -0500

    fix: pone readme como index y corrige linting de api.py

--- app/api.py
+++ app/api.py
@@ -4,25 +4,35 @@ import joblib
 import pandas as pd
 
 
+
 model = joblib.load("app/model.joblib")
+
+
+
 app = FastAPI(title="API de Riesgo Cardiovascular")
+
+
 
 class Input(BaseModel):
     features: list
 
+
+
 COLUMNAS = [
-    "Age", "Sex", "ChestPainType", "RestingBP", "Cholesterol", 
+    "Age", "Sex", "ChestPainType", "RestingBP", "Cholesterol",
     "FastingBS", "RestingECG", "MaxHR", "ExerciseAngina", "Oldpeak", "ST_Slope"
 ]
 
+
 @app.post("/predict")
 def predict(data: Input):
+    
     df = pd.DataFrame([data.features], columns=COLUMNAS)
-    
-    # Predecimos
+
+    # Predecimos usando el DataFrame
     proba = model.predict_proba(df)[0][1]
-    
+
     return {
-        "heart_disease_probability": float(proba), 
+        "heart_disease_probability": float(proba),
         "prediction": int(proba > 0.5)
     }
